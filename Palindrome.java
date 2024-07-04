3c3f22c3961b0a94bbb4a3a0fcf16e92aa36a9d4
P-Valarmathi <valarmathi.personal@gmail.com>
2024-07-04 22:02:10 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+class Palindrome {
+    public boolean isPalindrome(int n) {
+    int r,sum=0,temp;    
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10; 
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum) {     
+   return true; 
+  }else    
+   return false;
+}  
+}
